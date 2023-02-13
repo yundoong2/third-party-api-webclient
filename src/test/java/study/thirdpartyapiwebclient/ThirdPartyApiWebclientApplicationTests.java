08388c241af5c8b9605ca45f0508748a0dc8fbf5
YHCHOI <cyh6809@gmail.com>
2023-02-13 23:59:12 +0900

    Init

---
+++ src/test/java/study/thirdpartyapiwebclient/ThirdPartyApiWebclientApplicationTests.java
@@ -0,0 +1,13 @@
+package study.thirdpartyapiwebclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirdPartyApiWebclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
